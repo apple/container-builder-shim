10e1847d746489112acd01cd97e2dbef003cbbaf
Sidhartha Mani <wlan0@users.noreply.github.com>
2025-06-30 10:03:56 -0700

    Fix image resolution pipeline (#34)
    - pass the user specified image in the resolve request
    - default to linux/arm64 for cross platform builds

--- pkg/resolver/resolver.go
+++ pkg/resolver/resolver.go
@@ -21,12 +21,10 @@ import (
 	"fmt"
 
 	"github.com/containerd/containerd/platforms"
-	"github.com/containerd/containerd/reference"
 	"github.com/google/uuid"
 	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/client/llb/sourceresolver"
 	"github.com/opencontainers/go-digest"
-	"github.com/sirupsen/logrus"
 
 	"github.com/apple/container-builder-shim/pkg/api"
 	"github.com/apple/container-builder-shim/pkg/stream"
@@ -119,14 +117,6 @@ Response Format:
 */
 func (r *ResolverProxy) ResolveImageConfig(ctx context.Context, ref string, opt sourceresolver.Opt) (string, digest.Digest, []byte, error) {
 	var err error
-	u, err := reference.Parse(ref)
-	if err != nil {
-		logrus.Debugf("reference parse error: %v", err)
-		return ref, digest.Digest(""), nil, err
-	} else if u.Object == "" {
-		logrus.Debugf("reference object empty: %#v", u)
-		return ref, digest.Digest(""), nil, reference.ErrObjectRequired
-	}
 
 	req := &api.ImageTransfer{
 		Direction: api.TransferDirection_INTO,
